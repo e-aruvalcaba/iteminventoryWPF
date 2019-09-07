13ab89783bb4e7466237143b3a20227a368c1933
Adrian Ruvalcaba <adrian.ruvalcaba.sistemas@hotmail.com>
2019-09-07 15:44:50 -0500

    1st Project Structure

---
+++ ItemInventoryApp/Operations/UIRuntime.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ItemInventoryApp.Operations
+{
+    class UIRuntime
+    {
+        //public Border[] CreateBorders(List<string> dataArray)
+        //{
+        //    Border[] borderRet;
+
+        //    var loop = 0;
+        //    foreach (var item in dataArray)
+        //    {
+
+        //        Thickness tk = new Thickness(1.0);
+        //        Border bd = new Border();
+        //        bd.Width = 100;
+        //        bd.Height = 100;
+        //        bd.BorderBrush = Brushes.Black;
+        //        bd.BorderThickness = tk;
+        //        bd.Name = "border1";
+        //        Thickness margin = new Thickness(loop * 100, 0, 0, 0);
+
+
+
+        //        bd.Margin = margin;
+
+
+        //    }
+
+
+        //    return borderRet;
+        //}
+    }
+}
